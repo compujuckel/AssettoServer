d116aa6c69a98b0d7407fb642f23a3f02c1ece08
compujuckel <github@mackeben.eu>
2022-02-13 20:31:34 +0100

    fix warnings

--- AssettoServer/Server/Ai/TrafficConfiguration.cs
+++ AssettoServer/Server/Ai/TrafficConfiguration.cs
@@ -6,22 +6,22 @@ namespace AssettoServer.Server.Ai;
 [UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class TrafficConfiguration
 {
-    public List<SplineConfiguration> Splines { get; set; }
+    public List<SplineConfiguration> Splines { get; set; } = new();
 }
 
 [UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class SplineConfiguration
 {
-    public string Name { get; set; }
-    public List<JunctionRecord> Junctions { get; set; }
+    public string Name { get; set; } = "";
+    public List<JunctionRecord> Junctions { get; set; } = new();
 }
 
 [UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class JunctionRecord
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Start { get; set; }
-    public string EndSpline { get; set; }
+    public string EndSpline { get; set; } = "";
     public int End { get; set; }
     public float Probability { get; set; }
 }

ab2e4de7cc25f4e03fa3bea2b332a2d61d75756f
compujuckel <github@mackeben.eu>
2022-05-17 20:56:59 +0200

    new timing grpc contracts

--- AssettoServer.Hub.Contracts/ITimingLeaderboardClient.cs
+++ AssettoServer.Hub.Contracts/ITimingLeaderboardClient.cs
@@ -35,6 +35,65 @@ public class RegisterTimingLapTimeRequest
     public int LapTime { get; set; }
     [DataMember(Order = 6)]
     public string Car { get; set; } = "";
+    [DataMember(Order = 7)]
+    public bool Valid { get; set; } = true;
+}
+
+[DataContract]
+public class TimingLeaderboardRequest
+{
+    [DataMember(Order = 1)]
+    public string Leaderboard { get; set; } = "";
+    [DataMember(Order = 2)]
+    public string Track { get; set; } = "";
+    [DataMember(Order = 3)]
+    public string TimedStage { get; set; } = "";
+    [DataMember(Order = 4)]
+    public string? CarModel { get; set; }
+}
+
+[DataContract]
+public class TimingLeaderboardEntry
+{
+    [DataMember(Order = 1)]
+    public string Name { get; set; } = null!;
+    [DataMember(Order = 2)]
+    public int LapTime { get; set; }
+    [DataMember(Order = 3)]
+    public string CarModel { get; set; }= null!;
+}
+
+[DataContract]
+public class TimingLeaderboardResponse
+{
+    [DataMember(Order = 1)] 
+    public IEnumerable<TimingLeaderboardEntry> Entries { get; set; } = new List<TimingLeaderboardEntry>();
+}
+
+[DataContract]
+public class TimingPersonalBestRequest
+{
+    [DataMember(Order = 1)]
+    public string Leaderboard { get; set; } = "";
+    [DataMember(Order = 2)]
+    public string Track { get; set; } = "";
+    [DataMember(Order = 3)]
+    public string TimedStage { get; set; } = "";
+    [DataMember(Order = 4)]
+    public ulong Guid { get; set; }
+    [DataMember(Order = 5)]
+    public string? CarModel { get; set; }
+}
+
+[DataContract]
+public class TimingPersonalBestResponse
+{
+    [DataMember(Order = 1)]
+    public int? LapTime { get; set; }
+    [DataMember(Order = 2)]
+    public int? Rank { get; set; }
+    [DataMember(Order = 3)]
+    public string CarModel { get; set; } = null!;
 }
 
 [ServiceContract]
@@ -48,4 +107,10 @@ public interface ITimingLeaderboardClient
 
     [OperationContract]
     public Task RegisterLapTime(RegisterTimingLapTimeRequest request, CallContext context = default);
+    
+    [OperationContract]
+    public Task<TimingLeaderboardResponse> GetLeaderboard(TimingLeaderboardRequest request, CallContext context = default);
+
+    [OperationContract]
+    public Task<TimingPersonalBestResponse> GetPersonalBest(TimingPersonalBestRequest request, CallContext context = default);
 }

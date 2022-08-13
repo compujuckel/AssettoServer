4dedfa953a6c975cdeb1fc9ee0bdcf5e574a538d
compujuckel <github@mackeben.eu>
2022-08-13 17:40:51 +0200

    update grpc whitelist contracts

--- AssettoServer.Hub.Contracts/IWhitelistClient.cs
+++ AssettoServer.Hub.Contracts/IWhitelistClient.cs
@@ -9,6 +9,15 @@ public class WhitelistSubscriptionResponse
 {
     [DataMember(Order = 1)]
     public IReadOnlyList<ulong> Entries { get; set; } = new List<ulong>();
+    [DataMember(Order = 2)]
+    public int Version { get; set; }
+}
+
+[DataContract]
+public class WhitelistSubscriptionRequest
+{
+    [DataMember(Order = 1)]
+    public int Version { get; set; }
 }
 
 [DataContract]
@@ -22,7 +31,7 @@ public class WhitelistAddRequest
 public interface IWhitelistClient
 {
     [OperationContract]
-    IAsyncEnumerable<WhitelistSubscriptionResponse> SubscribeAsync(CallContext context = default);
+    IAsyncEnumerable<WhitelistSubscriptionResponse> SubscribeAsync(WhitelistSubscriptionRequest request, CallContext context = default);
 
     [OperationContract]
     Task AddAsync(WhitelistAddRequest request, CallContext context = default);

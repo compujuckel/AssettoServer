c650f2a447e979d132a7b1d7a8323408de9944ab
compujuckel <github@mackeben.eu>
2022-08-13 17:54:05 +0200

    Fix silent weather manager crash when downloading track params fails

--- AssettoServer/Server/TrackParams/IniTrackParamsProvider.cs
+++ AssettoServer/Server/TrackParams/IniTrackParamsProvider.cs
@@ -3,64 +3,73 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using IniParser;
-using IniParser.Model;
 using Serilog;
-using Serilog.Core;
 
-namespace AssettoServer.Server.TrackParams
+namespace AssettoServer.Server.TrackParams;
+
+public class IniTrackParamsProvider : ITrackParamsProvider
 {
-    public class IniTrackParamsProvider : ITrackParamsProvider
+    private const string TrackParamsPath = "cfg/data_track_params.ini";
+
+    private const string RemoteTrackParamsUrl =
+        "https://raw.githubusercontent.com/ac-custom-shaders-patch/acc-extension-config/master/config/data_track_params.ini";
+        
+    private readonly HttpClient _httpClient;
+        
+    public IniTrackParamsProvider()
     {
-        private const string TrackParamsPath = "cfg/data_track_params.ini";
+        _httpClient = new HttpClient();
+        _httpClient.Timeout = TimeSpan.FromSeconds(10);
+    }
 
-        private const string RemoteTrackParamsUrl =
-            "https://raw.githubusercontent.com/ac-custom-shaders-patch/acc-extension-config/master/config/data_track_params.ini";
-        
-        private readonly HttpClient _httpClient;
-        
-        public IniTrackParamsProvider()
+    public async Task Initialize()
+    {
+        if (!File.Exists(TrackParamsPath))
         {
-            _httpClient = new HttpClient();
-        }
+            Log.Information("{Path} not found, downloading from GitHub...", TrackParamsPath);
 
-        public async Task Initialize()
-        {
-            if (!File.Exists(TrackParamsPath))
+            try
             {
-                Log.Information("{Path} not found, downloading from GitHub...", TrackParamsPath);
                 var response = await _httpClient.GetAsync(RemoteTrackParamsUrl);
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    throw new Exception(
-                        "Could not get track params from GitHub. Please download or create them manually.");
+                    Log.Error("Could not get track params from {TrackParamsUrl} ({StatusCode})", RemoteTrackParamsUrl, response.StatusCode);
                 }
-
-                await using var file = File.Create(TrackParamsPath);
-                var responseStream = await response.Content.ReadAsStreamAsync();
-                await responseStream.CopyToAsync(file);
-            }
-        }
-        
-        public TrackParams? GetParamsForTrack(string track)
-        {
-            var cleanTrack = track.Substring(track.LastIndexOf('/') + 1);
-            
-            var parser = new FileIniDataParser();
-            var data = parser.ReadFile(TrackParamsPath);
-
-            if (data.Sections.ContainsSection(cleanTrack))
-            {
-                return new TrackParams()
+                else
                 {
-                    Latitude = double.Parse(data[cleanTrack]["LATITUDE"]),
-                    Longitude = double.Parse(data[cleanTrack]["LONGITUDE"]),
-                    Name = data[cleanTrack]["NAME"],
-                    Timezone = data[cleanTrack]["TIMEZONE"]
-                };
+                    await using var file = File.Create(TrackParamsPath);
+                    var responseStream = await response.Content.ReadAsStreamAsync();
+                    await responseStream.CopyToAsync(file);
+                }
             }
-
-            return null;
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Coould not get track params from {TrackParamsUrl}", RemoteTrackParamsUrl);
+            }
         }
+    }
+        
+    public TrackParams? GetParamsForTrack(string track)
+    {
+        if (!File.Exists(TrackParamsPath)) return null;
+            
+        var cleanTrack = track.Substring(track.LastIndexOf('/') + 1);
+            
+        var parser = new FileIniDataParser();
+        var data = parser.ReadFile(TrackParamsPath);
+
+        if (data.Sections.ContainsSection(cleanTrack))
+        {
+            return new TrackParams()
+            {
+                Latitude = double.Parse(data[cleanTrack]["LATITUDE"]),
+                Longitude = double.Parse(data[cleanTrack]["LONGITUDE"]),
+                Name = data[cleanTrack]["NAME"],
+                Timezone = data[cleanTrack]["TIMEZONE"]
+            };
+        }
+
+        return null;
     }
 }

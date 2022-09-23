37172efdf91d84b41fdf459350f860e6c5770f50
compujuckel <github@mackeben.eu>
2022-09-23 17:23:02 +0200

    add small helper for Lua object serialization

---
+++ AssettoServer/Utils/Luaon.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Luaon.Json;
+using Newtonsoft.Json;
+
+namespace AssettoServer.Utils;
+
+public static class Luaon
+{
+    public static string Serialize(object obj)
+    {
+        var serializer = new JsonSerializer();
+        using var sw = new StringWriter();
+        using (var jlw = new JsonLuaWriter(sw) { CloseOutput = false, Formatting = Formatting.None })
+        {
+            serializer.Serialize(jlw, obj);
+        }
+        return sw.ToString();
+    }
+}

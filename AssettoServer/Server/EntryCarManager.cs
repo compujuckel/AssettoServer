87225c1774ddf880357b551314a1848fe9ca2c44
compujuckel <github@mackeben.eu>
2024-01-14 13:19:26 +0100

    Fix player disconnect messages not showing up properly

--- AssettoServer/Server/EntryCarManager.cs
+++ AssettoServer/Server/EntryCarManager.cs
@@ -129,7 +129,7 @@ public class EntryCarManager
                 client.EntryCar.Client = null;
                 client.IsConnected = false;
 
-                if (client.ChecksumStatus != ChecksumStatus.Succeeded)
+                if (client.ChecksumStatus == ChecksumStatus.Succeeded)
                     BroadcastPacket(new CarDisconnected { SessionId = client.SessionId });
 
                 client.EntryCar.Reset();

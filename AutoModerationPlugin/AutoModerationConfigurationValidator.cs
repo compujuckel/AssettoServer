635506812fd20579fb4f6768adbde687cffde1b5
Nico <53304086+thisguyStan@users.noreply.github.com>
2024-01-19 03:08:44 +0100

    Updated validator

--- AutoModerationPlugin/AutoModerationConfigurationValidator.cs
+++ AutoModerationPlugin/AutoModerationConfigurationValidator.cs
@@ -12,16 +12,20 @@ public class AutoModerationConfigurationValidator : AbstractValidator<AutoModera
         {
             wwk.RuleFor(w => w.DurationSeconds).GreaterThanOrEqualTo(0);
             wwk.RuleFor(w => w.MinimumSpeedKph).GreaterThanOrEqualTo(0);
+            wwk.RuleFor(w => w.PitsBeforeKick).GreaterThanOrEqualTo(0);
         });
         RuleFor(cfg => cfg.NoLightsPenalty).NotNull().ChildRules(nlk =>
         {
             nlk.RuleFor(n => n.DurationSeconds).GreaterThanOrEqualTo(0);
             nlk.RuleFor(n => n.MinimumSpeedKph).GreaterThanOrEqualTo(0);
+            nlk.RuleFor(n => n.PitsBeforeKick).GreaterThanOrEqualTo(0);
+            nlk.RuleFor(n => n.IgnoreSeconds).GreaterThanOrEqualTo(0);
         });
         RuleFor(cfg => cfg.BlockingRoadPenalty).NotNull().ChildRules(brk =>
         {
             brk.RuleFor(b => b.DurationSeconds).GreaterThanOrEqualTo(0);
             brk.RuleFor(b => b.MaximumSpeedKph).GreaterThanOrEqualTo(0);
+            brk.RuleFor(b => b.PitsBeforeKick).GreaterThanOrEqualTo(0);
         });
     }
 }

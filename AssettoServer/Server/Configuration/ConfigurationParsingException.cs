4d1018b7a58e604972a0f59de72d22ebd7312e58
compujuckel <github@mackeben.eu>
2024-01-21 19:40:38 +0100

    - do not override minimum CSP version if version in server_cfg is higher than extra_cfg - always print file path on config file parsing errors - improve exception messages

---
+++ AssettoServer/Server/Configuration/ConfigurationParsingException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AssettoServer.Server.Configuration;
+
+public class ConfigurationParsingException : Exception
+{
+    public string? Path { get; }
+    
+    public ConfigurationParsingException()
+    {
+    }
+
+    public ConfigurationParsingException(string? path, Exception? inner = null)
+        : base($"Error parsing configuration file {path}", inner)
+    {
+        Path = path;
+    }
+}

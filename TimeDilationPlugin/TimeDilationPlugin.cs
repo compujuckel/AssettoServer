a5a5145362d0d643c5016a866a376d8a2562ab60
thisguyStan <53304086+thisguyStan@users.noreply.github.com>
2024-02-03 23:51:30 +0100

    Implemented time base time dilation

--- TimeDilationPlugin/TimeDilationPlugin.cs
+++ TimeDilationPlugin/TimeDilationPlugin.cs
@@ -1,4 +1,5 @@
-﻿using AssettoServer.Server.Configuration;
+﻿using System.Globalization;
+using AssettoServer.Server.Configuration;
 using AssettoServer.Server.Plugin;
 using AssettoServer.Server.Weather;
 using AssettoServer.Shared.Services;
@@ -10,15 +11,15 @@ namespace TimeDilationPlugin;
 
 public class TimeDilationPlugin : CriticalBackgroundService, IAssettoServerAutostart
 {
-    private readonly LookupTable _lookupTable;
     private readonly WeatherManager _weatherManager;
-    private readonly ACServerConfiguration _configuration;
+    private readonly ACServerConfiguration _serverConfiguration;
+    private readonly TimeDilationConfiguration _configuration;
 
     public TimeDilationPlugin(TimeDilationConfiguration configuration, WeatherManager weatherManager, ACServerConfiguration serverConfiguration, IHostApplicationLifetime applicationLifetime) : base(applicationLifetime)
     {
         _weatherManager = weatherManager;
-        _configuration = serverConfiguration;
-        _lookupTable = new LookupTable(configuration.LookupTable.Select(entry => new KeyValuePair<double, double>(entry.SunAngle, entry.TimeMult)).ToList());
+        _serverConfiguration = serverConfiguration;
+        _configuration = configuration;
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -28,13 +29,53 @@ public class TimeDilationPlugin : CriticalBackgroundService, IAssettoServerAutos
             Log.Error("TimeDilationPlugin cannot get current sun position, aborting");
             return;
         }
+
+        if (_configuration.Mode == TimeDilationMode.Time)
+            await TimeBasedTimeDilation(stoppingToken);
+        else
+            await SunPositionTimeDilation(stoppingToken);
+
+    }
+
+    private async Task SunPositionTimeDilation(CancellationToken stoppingToken)
+    {
+        var lookupTable = new LookupTable(_configuration.SunAngleLookupTable.
+            Select(entry => new KeyValuePair<double, double>(entry.SunAngle, entry.TimeMult)).ToList());
         
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                double sunAltitudeDeg = _weatherManager.CurrentSunPosition.Value.Altitude * 180.0 / Math.PI;
-                _configuration.Server.TimeOfDayMultiplier = (float)_lookupTable.GetValue(sunAltitudeDeg);
+                double sunAltitudeDeg = _weatherManager.CurrentSunPosition!.Value.Altitude * 180.0 / Math.PI;
+                _serverConfiguration.Server.TimeOfDayMultiplier = (float)lookupTable.GetValue(sunAltitudeDeg);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error during time dilation update");
+            }
+            finally
+            {
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+
+    private async Task TimeBasedTimeDilation(CancellationToken stoppingToken)
+    {
+        var lookupTable = new LookupTable(_configuration.TimeLookupTable
+            .Select(entry => 
+                new KeyValuePair<double, double>(
+                    DateTime.ParseExact(entry.Time, "H:mm", CultureInfo.InvariantCulture).TimeOfDay.TotalSeconds, 
+                    entry.TimeMult)
+            ).ToList());
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                var liveTime = _weatherManager.CurrentDateTime.TimeOfDay;
+                double currentTime = new TimeSpan(liveTime.Hour, liveTime.Minute, liveTime.Second).TotalSeconds;
+                _serverConfiguration.Server.TimeOfDayMultiplier = (float)lookupTable.GetValue(currentTime);
             }
             catch (Exception ex)
             {

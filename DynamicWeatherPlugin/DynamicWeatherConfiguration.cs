8601942d8a4d1eab6b213f027d6bc2dad0cce836
thisguyStan <53304086+thisguyStan@users.noreply.github.com>
2024-02-06 14:26:11 +0100

    Starting Dynamic Weather Stuff

---
+++ DynamicWeatherPlugin/DynamicWeatherConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using AssettoServer.Server.Configuration;
+using AssettoServer.Shared.Weather;
+using JetBrains.Annotations;
+using YamlDotNet.Serialization;
+
+namespace DynamicWeatherPlugin;
+
+[UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
+public class DynamicWeatherConfiguration : IValidateConfiguration<DynamicWeatherConfigurationValidator>
+{
+    [YamlMember(Description = "Weights for weather transition, only listed weathers will be counted, setting a weight to 0 blacklists a weather")]
+    public Dictionary<WeatherFxType, Dictionary<WeatherFxType, float>> WeatherTransitions { get; init; } = new();
+
+    [YamlMember(Description = "Minimum duration until next weather change")]
+    public int MinWeatherDurationMinutes { get; set; } = 5;
+    [YamlMember(Description = "Maximum duration until next weather change")]
+    public int MaxWeatherDurationMinutes { get; set; } = 30;
+
+    [YamlMember(Description = "Minimum weather transition duration")]
+    public int MinTransitionDurationSeconds { get; set; } = 120;
+    [YamlMember(Description = "Maximum weather transition duration")]
+    public int MaxTransitionDurationSeconds { get; set; } = 600;
+
+    [YamlIgnore] public int MinWeatherDurationMilliseconds => MinWeatherDurationMinutes * 60_000;
+    [YamlIgnore] public int MaxWeatherDurationMilliseconds => MaxWeatherDurationMinutes * 60_000;
+    [YamlIgnore] public int MinTransitionDurationMilliseconds => MinTransitionDurationSeconds * 1_000;
+    [YamlIgnore] public int MaxTransitionDurationMilliseconds => MaxTransitionDurationSeconds * 1_000;
+}
---
+++ DynamicWeatherPlugin/DynamicWeatherModule.cs
@@ -0,0 +1,12 @@
+﻿using AssettoServer.Server.Plugin;
+using Autofac;
+
+namespace DynamicWeatherPlugin;
+
+public class DynamicWeatherModule : AssettoServerModule<DynamicWeatherConfiguration>
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DynamicWeather>().AsSelf().As<IAssettoServerAutostart>().SingleInstance();
+    }
+}

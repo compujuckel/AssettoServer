b84918caae7d0708cf0d0c5848c2bac3929d52b5
thisguyStan <53304086+thisguyStan@users.noreply.github.com>
2024-03-05 21:12:23 +0100

    RandomWeather reworked default behavior for weathers

--- RandomWeatherPlugin/RandomWeatherConfiguration.cs
+++ RandomWeatherPlugin/RandomWeatherConfiguration.cs
@@ -9,12 +9,8 @@ namespace RandomWeatherPlugin;
 public class RandomWeatherConfiguration : IValidateConfiguration<RandomWeatherConfigurationValidator>
 {
     [YamlMember(Description = "Which mode should be used for weather randomization \nAvailable values: 'Default' and 'TransitionTable'")]
-    public RandomWeatherMode Mode = RandomWeatherMode.Default;
-    [YamlMember(Description = "Weights for random weather selection, setting a weight to 0 blacklists a weather, default weight is 1")]
-    public Dictionary<WeatherFxType, float> WeatherWeights { get; init; } = new();
-    [YamlMember(Description = "Weights for weather transition, only listed weathers will be counted")]
-    public Dictionary<WeatherFxType, Dictionary<WeatherFxType, float>> WeatherTransitions { get; init; } = new();
-
+    public RandomWeatherMode Mode { get; set; } = RandomWeatherMode.Default;
+    
     [YamlMember(Description = "Minimum duration until next weather change")]
     public int MinWeatherDurationMinutes { get; set; } = 5;
     [YamlMember(Description = "Maximum duration until next weather change")]
@@ -24,6 +20,46 @@ public class RandomWeatherConfiguration : IValidateConfiguration<RandomWeatherCo
     public int MinTransitionDurationSeconds { get; set; } = 120;
     [YamlMember(Description = "Maximum weather transition duration")]
     public int MaxTransitionDurationSeconds { get; set; } = 600;
+    
+    [YamlMember(Description = "Weights for weather transition, only listed weathers will be counted\nCheck the reference config to see the structure")]
+    public Dictionary<WeatherFxType, Dictionary<WeatherFxType, float>> WeatherTransitions { get; init; } = new();
+    [YamlMember(Description = "Weights for random weather selection, removing a weight or setting it to 0 blacklists a weather\nYou can also use decimals like 0.1")]
+    public Dictionary<WeatherFxType, float> WeatherWeights { get; init; } = new()
+    {
+        { WeatherFxType.LightThunderstorm, 1.0f },
+        { WeatherFxType.Thunderstorm, 1.0f },
+        { WeatherFxType.HeavyThunderstorm, 1.0f },
+        { WeatherFxType.LightDrizzle, 1.0f },
+        { WeatherFxType.Drizzle, 1.0f },
+        { WeatherFxType.HeavyDrizzle, 1.0f },
+        { WeatherFxType.LightRain, 1.0f },
+        { WeatherFxType.Rain, 1.0f },
+        { WeatherFxType.HeavyRain, 1.0f },
+        { WeatherFxType.LightSnow, 1.0f },
+        { WeatherFxType.Snow, 1.0f },
+        { WeatherFxType.HeavySnow, 1.0f },
+        { WeatherFxType.LightSleet, 1.0f },
+        { WeatherFxType.Sleet, 1.0f },
+        { WeatherFxType.HeavySleet, 1.0f },
+        { WeatherFxType.Clear, 1.0f },
+        { WeatherFxType.FewClouds, 1.0f },
+        { WeatherFxType.ScatteredClouds, 1.0f },
+        { WeatherFxType.BrokenClouds, 1.0f },
+        { WeatherFxType.OvercastClouds, 1.0f },
+        { WeatherFxType.Fog, 1.0f },
+        { WeatherFxType.Mist, 1.0f },
+        { WeatherFxType.Smoke, 1.0f },
+        { WeatherFxType.Haze, 1.0f },
+        { WeatherFxType.Sand, 1.0f },
+        { WeatherFxType.Dust, 1.0f },
+        { WeatherFxType.Squalls, 1.0f },
+        { WeatherFxType.Tornado, 1.0f },
+        { WeatherFxType.Hurricane, 1.0f },
+        { WeatherFxType.Cold, 1.0f },
+        { WeatherFxType.Hot, 1.0f },
+        { WeatherFxType.Windy, 1.0f },
+        { WeatherFxType.Hail, 1.0f },
+    };
 
     [YamlIgnore] public int MinWeatherDurationMilliseconds => MinWeatherDurationMinutes * 60_000;
     [YamlIgnore] public int MaxWeatherDurationMilliseconds => MaxWeatherDurationMinutes * 60_000;
@@ -33,6 +69,6 @@ public class RandomWeatherConfiguration : IValidateConfiguration<RandomWeatherCo
 
 public enum RandomWeatherMode
 {
-    Default,
-    TransitionTable
+    Default = 0,
+    TransitionTable = 1
 }

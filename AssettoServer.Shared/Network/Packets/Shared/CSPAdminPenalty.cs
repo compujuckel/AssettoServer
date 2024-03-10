1fab95fe75bdd5026b4c66d37edf76efc1238a5e
Nico <53304086+thisguyStan@users.noreply.github.com>
2024-03-10 16:00:22 +0100

    Added csp admin penalty support (#85)
    * Made restrictor and ballast commands csp admin tool compatible
    
    * First attempt to implement Admin Penalty
    
    * CSP Penalty works with server session id
    
    * Read Packet instead of passthrough, targeted send
    
    * Fixed signature
    
    * Removed commented code
    
    * Reverted log to original level...

---
+++ AssettoServer.Shared/Network/Packets/Shared/CSPAdminPenalty.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using AssettoServer.Shared.Network.Packets.Incoming;
+using AssettoServer.Shared.Network.Packets.Outgoing;
+
+namespace AssettoServer.Shared.Network.Packets.Shared;
+
+public class CSPAdminPenalty : IIncomingNetworkPacket, IOutgoingNetworkPacket
+{
+    public byte SessionId;
+    public CSPAdminPenaltyMode Mode;
+    public ushort CarIndex;
+    public int PenaltyArgument;
+    public string Message;
+    public ulong Signature;
+
+    public void FromReader(PacketReader reader)
+    {
+        Mode = (CSPAdminPenaltyMode) reader.Read<ushort>();
+        CarIndex = reader.Read<ushort>();
+        PenaltyArgument = reader.Read<int>();
+        Message = reader.ReadStringFixed(Encoding.UTF8, 64);        
+        Signature = reader.Read<ulong>();
+    }
+
+    public void ToWriter(ref PacketWriter writer)
+    {
+        writer.Write((byte)ACServerProtocol.Extended);
+        writer.Write((byte)CSPMessageTypeTcp.ClientMessage);
+        writer.Write(SessionId);
+        writer.Write((ushort)CSPClientMessageType.AdminPenalty);
+        
+        writer.Write((ushort)Mode);
+        writer.Write(CarIndex);
+        writer.Write(PenaltyArgument);
+        
+        writer.WriteString(Message, Encoding.UTF8, 64);
+        writer.Write(Signature);
+    }
+}
+
+/// <summary>
+/// Only <b>MandatoryPits</b> and <b>TeleportToPits</b> are known to work
+/// </summary>
+public enum CSPAdminPenaltyMode
+{
+    None = 0,
+    MandatoryPits = 1,
+    TeleportToPits = 2,
+    SlowDown = 3,
+    BlackFlag = 4,
+    BlackFlagRelease = 5
+}
--- AssettoServer/Network/CSPClientMessageHandler.cs
+++ AssettoServer/Network/CSPClientMessageHandler.cs
@@ -72,6 +72,11 @@ public class CSPClientMessageHandler(CSPClientMessageTypeManager cspClientMessag
                 OnHandshakeOut(sender, reader);
                 break;
             }
+            case CSPClientMessageType.AdminPenalty:
+            {
+                OnAdminPenaltyOut(sender, reader);
+                break;
+            }
             default:
             {
                 if (_cspClientMessageTypeManager.RawMessageTypes.TryGetValue(packetType, out var handler))
@@ -159,6 +164,26 @@ public class CSPClientMessageHandler(CSPClientMessageTypeManager cspClientMessag
 
         sender.Logger.Information("CSP handshake received from {ClientName} ({SessionId}): Version={Version} WeatherFX={WeatherFxActive} InputMethod={InputMethod} RainFX={RainFxActive} HWID={HardwareId}", 
             sender.Name, sender.SessionId, packet.Version, packet.IsWeatherFxActive, packet.InputMethod, packet.IsRainFxActive, packet.UniqueKey);
+    }
+
+    private void OnAdminPenaltyOut(ACTcpClient sender, PacketReader reader)
+    {
+
+        if (sender.IsAdministrator)
+        {
+            CSPAdminPenalty packet = reader.ReadPacket<CSPAdminPenalty>();
+            packet.SessionId = 255;
+            
+            sender.Logger.Information("CSP admin penalty received from {ClientName} ({SessionId}): User is admin", 
+                sender.Name, sender.SessionId);
+            
+            _entryCarManager.EntryCars[packet.CarIndex].Client?.SendPacket(packet);
+        }
+        else
+        {
+            sender.Logger.Information("CSP admin penalty received from {ClientName} ({SessionId}): User is not admin", 
+                sender.Name, sender.SessionId);
+        }
     }
 
     private static bool IsRanged(CSPClientMessageType type)

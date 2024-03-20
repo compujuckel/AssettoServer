373ac81107c40d0852f589dd89bda264dfa23b3c
thisguyStan <53304086+thisguyStan@users.noreply.github.com>
2024-03-20 02:09:47 +0100

    Implemented custom chat commands as a plugin

---
+++ CustomCommandPlugin/CustomCommand.cs
@@ -0,0 +1,47 @@
+﻿using AssettoServer.Commands;
+using AssettoServer.Commands.Contexts;
+using AssettoServer.Server.Plugin;
+using AssettoServer.Shared.Services;
+using Microsoft.Extensions.Hosting;
+using Qmmands;
+using Serilog;
+
+namespace CustomCommandPlugin;
+
+public class CustomCommand : CriticalBackgroundService, IAssettoServerAutostart
+{
+    public CustomCommand(CustomCommandConfiguration configuration, CommandService commandService, IHostApplicationLifetime applicationLifetime) : base(applicationLifetime)
+    {
+        commandService.AddModule<ACModuleBase>(m =>
+        {
+            foreach (var (alias, response) in configuration.Commands)
+            {
+                if (!commandService.FindCommands(alias).Any(c => c.Command.Aliases.Any(a => a == alias)))
+                {
+                    m.AddCommand(ctx =>
+                    {
+                        Callback((BaseCommandContext) ctx);
+                    }, c =>
+                    {
+                        c.Aliases.Add(alias);
+                    });
+
+                    void Callback(BaseCommandContext c)
+                    {
+                        c.Reply(response);
+                    }
+                }
+                else
+                {
+                    Log.Warning("Command {Alias} does already exist, please use something else", alias);
+                }
+            }
+        });
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Log.Debug("CustomCommandPlugin autostart called");
+        return Task.CompletedTask;
+    }
+}
---
+++ CustomCommandPlugin/CustomCommandConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AssettoServer.Server.Configuration;
+using JetBrains.Annotations;
+using YamlDotNet.Serialization;
+
+namespace CustomCommandPlugin;
+
+[UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
+public class CustomCommandConfiguration
+{
+    [YamlMember(Description = "Configure your custom commands")]
+    public Dictionary<string, string> Commands { get; init; } = new()
+    {
+        {"discord", "https://discord.gg/uXEXRcSkyz"}
+    };
+}

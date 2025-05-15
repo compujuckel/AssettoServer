6f7af2d723a3af4c7658e2c2a8ca9558f9f9f9c1
compujuckel <compujuckel@users.noreply.github.com>
2025-05-15 20:44:09 +0200

    Replace Nerdbank.GitVersioning with MinVer, as it better fits the workflow update dependencies

---
+++ AssettoServer/ThisAssembly.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssettoServer;
+
+internal static class ThisAssembly
+{
+    internal static string AssemblyInformationalVersion { get; }
+    internal static string? GitCommitHash { get; }
+
+    static ThisAssembly()
+    {
+        var informationalVersion = GetAssemblyAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        var plusIndex = informationalVersion.IndexOf('+');
+        if (plusIndex > 0)
+        {
+            AssemblyInformationalVersion = informationalVersion[..plusIndex];
+            GitCommitHash = informationalVersion[(plusIndex + 1)..];
+        }
+        else
+        {
+            AssemblyInformationalVersion = informationalVersion;
+        }
+    }
+
+    private static T GetAssemblyAttribute<T>() where T : Attribute =>
+        (T)Attribute.GetCustomAttribute(typeof(ThisAssembly).Assembly, typeof(T), false)!;
+}

19ef6188b97e3834253ac899571b8365e7c7e3b1
Nico <53304086+thisguyStan@users.noreply.github.com>
2025-06-07 13:42:08 +0200

    New Plugin: Tag Game Mode (#128)
    * works but not final
    
    * Fixed client crash
    
    * improvement for first update sent
    
    * Make OnFirstUpdateSent async and add delay
    
    * Added change in default behavior for late joiners
    
    * Added different win condition
    
    * Small adjustment to alternative winning condition
    
    * Refactor to use color instead of bytes for each color part
    
    * Removed debug stuff
    
    * minor improvements
    
    * Added broadcast for initial tagger
    
    * Merged start commands
    
    * random++

---
+++ TagModePlugin/TagModeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AssettoServer.Server.Configuration;
+using AssettoServer.Shared.Weather;
+using JetBrains.Annotations;
+using YamlDotNet.Serialization;
+
+namespace TagModePlugin;
+
+[UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
+public class TagModeConfiguration : IValidateConfiguration<TagModeConfigurationValidator>
+{
+    [YamlMember(Description = "Should a session only end when all runners have been caught.\nIf set to true, the winner will be the last runner that was caught.\nIf set to false, if at least one runner was not caught when the time runs out, the runners win")]
+    public bool EnableEndlessMode { get; init; } = false;
+    [YamlMember(Description = "Should tag sessions be started automatically")]
+    public bool EnableLoop { get; init; } = false;
+    [YamlMember(Description = "If this is set to 'true' late joiners will join an active game as a runner. \nIf this is set to 'false' late joiners will join an active game as a tagger")]
+    public bool EnableLateJoinRunner { get; init; } = true;
+    [YamlMember(Description = "How long a session last, if all players are tagged, the session also ends")]
+    public int SessionDurationMinutes { get; init; } = 5;
+    [YamlMember(Description = "How long the pause between sessions is")]
+    public int SessionPauseIntervalMinutes { get; init; } = 2;
+    
+    [YamlMember(Description = "Color used when no session is active")]
+    public string NeutralColor { get; init; } = "#348feb";
+    [YamlMember(Description = "Color used when running from the taggers")]
+    public string RunnerColor { get; init; } = "#dbdbdb";
+    [YamlMember(Description = "Color when hit by the tagger/joining the taggers")]
+    public string TaggedColor { get; init; } = "#fa0000";
+
+    [YamlIgnore] public int SessionPauseIntervalMilliseconds => SessionPauseIntervalMinutes * 60_000;
+    [YamlIgnore] public long SessionDurationMilliseconds => SessionDurationMinutes * 60_000;
+}

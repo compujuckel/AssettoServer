6d01c085f72e78cc1c63898c916ee076f32a9309
compujuckel <compujuckel@users.noreply.github.com>
2026-03-21 19:39:19 +0100

    - update default min CSP version to 0.2.0 - when serializing yaml, use flow style (inline) for Vector3

---
+++ AssettoServer/Utils/YamlFlowStyleEmitter.cs
@@ -0,0 +1,19 @@
+﻿using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.EventEmitters;
+
+namespace AssettoServer.Utils;
+
+public class YamlFlowStyleEmitter<T>(IEventEmitter next) : ChainedEventEmitter(next)
+{
+    public override void Emit(MappingStartEventInfo eventInfo, IEmitter emitter)
+    {
+        if (eventInfo.Source.Type == typeof(T))
+        {
+            eventInfo.Style = MappingStyle.Flow;
+        }
+
+        base.Emit(eventInfo, emitter);
+    }
+}
